e907b96fe2f234c552533f1f1ba0e7d3318c6173
Salome Oumarou Wate <94982651+OumarouWatS@users.noreply.github.com>
2022-08-30 20:50:09 -0500

    Create index.java

---
+++ index.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import com.sun.source.tree.WhileLoopTree;
+class Main {
+  // This is the Leap Year Calculator
+  public static void main(String[] args) {
+    int year;
+    String decision;
+    System.out.println("Hello world!");
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("Please, enter the year in order to determine the type:");
+    year = scnr.nextInt();
+    do {
+      if((year%400 == 0 || year%100 == 0) && (year % 4 == 0)){
+          System.out.println("The year is a leap year. It has 366 days.");
+        } else {
+    System.out.println("The year is not a leap year, it has 365 days.");
+}
+      System.out.println("Do you want to keep checking years? Please, answer by Yes or No");
+decision = scnr.nextLine();
+} while (decision == "Yes");
+    System.out.println("Goodbye!!!");
+    scnr.close();
+}
+}
